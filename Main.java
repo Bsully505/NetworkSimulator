313d2f3cc066d00bdaccc57520fccce85b291df4
Bryan Sullivan <Bsully505@yahoo.com>
2021-03-21 17:28:22 -0400

    working on ping package

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         if (args.length > 0) {
             networkFile = args[0];
         }
-        Debug.getInstance().setLevel(1);  // Set debug level for more verbose output (higher = more verbose)
+        Debug.getInstance().setLevel(5);  // Set debug level for more verbose output (higher = more verbose)
         
         Network net = new Network();
         try {
@@ -17,7 +17,8 @@ public class Main {
         }
 
         net.printNetwork(System.out);
-        net.createRouters(new FloodRouter.Generator());
+        //net.createRouters(new FloodRouter.Generator());
+        net.createRouters(new LinkStateRouter.Generator());
         
         try {
             net.runNetwork(System.out, 10000, 1, 0.5);
